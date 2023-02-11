71841adcc778bceebb45ee84615bac4597a0d1a7
Justyna Jachowicz <justyna.jachowicz@microsoft.wsei.edu.pl>
2023-02-11 12:32:25 +0100

    Initial commit.

---
+++ SpiralMatrix.py
@@ -0,0 +1,29 @@
+def spiralOrder(matrix: list[list[int]]) -> list[int]:
+
+    result = []
+    left, right = 0, len(matrix[0])
+    top, bottom = 0, len(matrix)
+
+    while left < right and top < bottom:
+        for i in range(left, right):
+            result.append(matrix[top][i])
+        top += 1
+
+        for i in range(top, bottom):
+            result.append(matrix[i][right - 1])
+        right -= 1
+
+        if not (left < right and top < bottom):
+            break
+
+        for i in range(right - 1, left - 1, -1):
+            result.append(matrix[bottom - 1][i])
+        bottom -= 1
+
+        for i in range(bottom - 1, top - 1, -1):
+            result.append(matrix[i][left])
+        left += 1
+
+    return result
+
+print(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
