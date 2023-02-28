19b92c4d605849886baf012d0e40aa221039a985
Justyna Jachowicz <just.jachowicz@gmail.com>
2023-02-28 22:11:48 +0100

    Initial commit.

--- ArrayPartition.py
+++ ArrayPartition.py
@@ -1,2 +1,10 @@
-def arrayPairSum(self, nums: list[int]) -> int:
-    pass
+def arrayPairSum(nums: list[int]) -> int:
+    sortedNums = sorted(nums)
+    sumOfNums = 0
+
+    for i in range(0, len(sortedNums), 2):
+         sumOfNums += min(sortedNums[i], sortedNums[i + 1])
+
+    return sumOfNums
+
+print(arrayPairSum([1,4,3,2]))
