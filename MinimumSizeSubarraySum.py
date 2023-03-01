c8d2fd4eefe42b1dbcd5e9cb0d76f06ee1c8ca2a
Justyna Jachowicz <just.jachowicz@gmail.com>
2023-03-01 22:57:24 +0100

    Initial commit.

---
+++ MinimumSizeSubarraySum.py
@@ -0,0 +1,12 @@
+def minSubArrayLen(target: int, nums: list[int]) -> int:
+    left, total = 0
+    result = float("inf")
+
+    for r in range(len(nums)):
+        total += nums[r]
+        while total >= target:
+            result = min(r - left + 1, result)
+            total -= nums[left]
+            left += 1
+
+    return 0 if result == float("inf") else result
